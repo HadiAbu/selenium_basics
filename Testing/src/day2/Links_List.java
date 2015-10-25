3c82caa2e57b3b8d4537037e5566b837efa052bd
Hadi Abu Hamed <hadi.abuhamed@galilsoftware.com>
2015-10-25 16:30:47 +0200

    ..

---
+++ Testing/src/day2/Links_List.java
@@ -0,0 +1,19 @@
+package day2;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class Links_List {
+	public static void main(String []args)
+	{
+		HtmlUnitDriver driver = new HtmlUnitDriver();
+		driver.get("http://www.bbc.com");
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		
+		System.out.println("Total links " + links.size());
+		
+	}
+}
--- Testing/src/day2/TimeOut.java
+++ Testing/src/day2/TimeOut.java
@@ -10,10 +10,12 @@ public class TimeOut {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//if the elemnt is not found. wait maximum 10 seconds.
+		//if the element is not found. wait maximum 10 seconds.
 		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+		
 		
 	}
 
